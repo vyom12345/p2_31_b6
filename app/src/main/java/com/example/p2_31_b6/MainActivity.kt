9190b9f97311b400db9bac600b8cde4c0361062f
vyom12345 <vyomprajapati19@gnu.ac.in>
2023-08-03 21:32:30 +0530

    p2

--- app/src/main/java/com/example/p2_31_b6/MainActivity.kt
+++ app/src/main/java/com/example/p2_31_b6/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.p2_31_b6
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import android.widget.Toast
 import com.google.android.material.snackbar.Snackbar
 import kotlin.math.log
@@ -11,6 +12,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         val TAG = "MainActivity"
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
@@ -18,30 +20,36 @@ class MainActivity : AppCompatActivity() {
     override fun onStart() {
         super.onStart()
         displaymessage("OnStart Called")
+        displays("Onstart called")
     }
 
     override fun onResume() {
         super.onResume()
         displaymessage("OnResume Called")
+        displays("OnResume called")
     }
 
     override fun onPause() {
         super.onPause()
         displaymessage("OnPause Called")
+        displays("OnPause called")
     }
 
     override fun onStop() {
         super.onStop()
         displaymessage("OnStop Called")
+        displays("OnStop called")
     }
 
     override fun onDestroy() {
         super.onDestroy()
         displaymessage("OnDestroy Called")
+        displays("OnDestroy called")
     }
 
     override fun onRestart() {
         displaymessage("OnRestart Called")
+        displays("OnRestart called")
         super.onRestart()
     }
 
@@ -49,8 +57,8 @@ class MainActivity : AppCompatActivity() {
         Toast.makeText(this,"This is $msg",Toast.LENGTH_LONG).show();
         Log.i("MainActivity","This is log message")
     }
-//    fun displaysnack(msg: String){
-//        Snackbar.make(findViewById(R.id.))
-//    }
-
+   fun displays(msg: String){
+       val myLayout = findViewById<View>(R.id.mylayout)
+       Snackbar.make(myLayout, "This is $msg", Snackbar.LENGTH_LONG).show()
+}
 }
