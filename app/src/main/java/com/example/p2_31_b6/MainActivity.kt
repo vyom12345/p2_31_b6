c1afed701f7fee3e5038f6ec5374646435362dc0
vyom12345 <vyomprajapati19@gnu.ac.in>
2023-08-03 21:36:24 +0530

    p2

--- app/src/main/java/com/example/p2_31_b6/MainActivity.kt
+++ app/src/main/java/com/example/p2_31_b6/MainActivity.kt
@@ -6,7 +6,6 @@ import android.util.Log
 import android.view.View
 import android.widget.Toast
 import com.google.android.material.snackbar.Snackbar
-import kotlin.math.log
 
 class MainActivity : AppCompatActivity() {
 
